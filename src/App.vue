3e7748aa6a0f2ee7c382b03ef7afd3c327a69b15
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2020-10-27 12:47:08 +0100

    implementation of userProfile component

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,32 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+	<div id="app">
+    <userProfile />
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import userProfile from "./components/userProfile"
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    userProfile
+  },
+};
 </script>
 
 <style>
+
+html, body{
+  height: 100%;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background: rgb(250, 250, 250);
+  display: flex;
+  flex-direction: column;
+  /* align-items: center; */
+	color: rgb(82, 80, 80);
+  height: 100%;
 }
 </style>
---
+++ src/components/userProfile.vue
@@ -0,0 +1,76 @@
+<template>
+    <div class="user-profile">
+        <div class="user-profile__user-panel">
+            <h1 class="user-profile__username">@{{user.username}} </h1>
+            <div class="user-profile__admin-badge" v-if="user.isAdmin && followers > 10">Admin</div>
+            <div class="user-profile__follower-count">
+                <strong>follower: {{ followers }} - </strong>
+                <button v-on:click="followUser" >Follow</button>
+            </div>
+        </div>
+    </div>
+
+    <div class="user-profile" v-for="twit in user.twits" :key="twit.id">
+        <div>{{twit.content}}</div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "userProfile",
+
+    data(){
+        return {
+            followers: 0,
+            user: {
+                id: 1,
+				followers: 0,
+				username: "Rx_Silanka",
+				firstName: "Paul",
+				lastName: "Onyekwelu",
+				email: "silanka007@gmail.com",
+                isAdmin: true,
+                twits: [
+                    {id: 1, content: "twitter is fun!"},
+                    {id: 2, content: "same is vue js"},
+                    {id: 3, content: " I prefer reactJs though :)"},
+                ]
+            }
+        }
+    },
+    computed: {
+        fullname() {
+            return `${this.user.firstName} ${this.user.lastName}`;
+        }
+    },
+    methods: {
+        followUser() {
+            this.followers++
+        }
+    }
+}
+</script>
+
+<style scoped>
+.user-profile{
+    background: white;
+    padding: 1rem;
+    box-shadow: 0px 0px 4px rgb(189, 189, 189);
+    width: 80vw;
+    max-width: 400px;
+}
+
+.user-profile__user-panel{
+    display: flex;
+    flex-direction: column;
+}
+
+.user-profile__admin-badge{
+    background: rebeccapurple;
+    color: white;
+    padding: 3px;
+    margin-right: auto;
+    border-radius: 5px;
+}
+
+</style>
